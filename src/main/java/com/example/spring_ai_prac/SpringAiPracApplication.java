31f5fbea830f57dbbc0fca051c1662a459c2b9d7
GyeongHun <riverpower6@naver.com>
2025-11-21 16:29:04 +0900

    [FEAT] Spring AI 실습 프로젝트 세팅 및 HTML 작성

---
+++ src/main/java/com/example/spring_ai_prac/SpringAiPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiPracApplication.class, args);
+    }
+
+}
